2b24dc5ac3874850124af0ce0fd118d0fafa3ee9
Joey Hotz <joey.hotz@eucalyptus.vc>
2023-12-01 10:13:12 +1100

    init

---
+++ gql.ts
@@ -0,0 +1,27 @@
+export const typeDefs = `#graphql
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+export const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+export const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
---
+++ server.ts
@@ -0,0 +1,18 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { resolvers, typeDefs } from "./gql";
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀 Server ready at: ${url}`);
+};
+
+startServer();
